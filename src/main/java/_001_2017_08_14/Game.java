96aba1ff9611df505a0e4a77438d7aa8055eaf9f
Dmytro Verbivskyi <dverbovskiy@gmail.com>
2017-08-15 00:05:33 +0300

    all 5 tests are done first week

--- src/main/java/_001_2017_08_14/Game.java
+++ src/main/java/_001_2017_08_14/Game.java
@@ -2,13 +2,49 @@ package _001_2017_08_14;
 
 class Game {
 
-    private int score;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
 
     int getScore() {
+        int score = 0;
+        int currentRoll = 0;
+
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(currentRoll)) {
+                score += 10 + strikeBonus(currentRoll);
+                currentRoll++;
+            } else if (isSpare(currentRoll)) {
+                score += 10 + spareBonus(currentRoll);
+                currentRoll += 2;
+            } else {
+                score += sumInFrame(currentRoll);
+                currentRoll += 2;
+            }
+        }
         return score;
     }
 
-    void roll(int pins) {
+    private int sumInFrame(int currentRoll) {
+        return rolls[currentRoll] + rolls[currentRoll + 1];
+    }
 
+    private int spareBonus(int currentRoll) {
+        return rolls[currentRoll + 2];
+    }
+
+    private int strikeBonus(int currentRoll) {
+        return rolls[currentRoll + 1] + rolls[currentRoll + 2];
+    }
+
+    private boolean isStrike(int currentRoll) {
+        return rolls[currentRoll] == 10;
+    }
+
+    private boolean isSpare(int currentRoll) {
+        return rolls[currentRoll] + rolls[currentRoll + 1] == 10;
+    }
+
+    void roll(int pins) {
+        rolls[currentRoll++] = pins;
     }
 }
